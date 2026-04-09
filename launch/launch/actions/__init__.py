5ed191edc8e2fd09a1ed0b3ca59960a6945ef447
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-04-09 11:19:56 +0200

    Revert "Trimmed the branch to only the launch_pytest sub directory"
    This reverts commit 3c76fecea7aecb503a9b23dad232ead29cfd9118.

---
+++ launch/launch/actions/__init__.py
@@ -0,0 +1,81 @@
+# Copyright 2018 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""actions Module."""
+
+from .declare_launch_argument import DeclareLaunchArgument
+from .append_environment_variable import AppendEnvironmentVariable  # noqa: I100
+from .emit_event import EmitEvent
+from .execute_local import ExecuteLocal
+from .execute_process import ExecuteProcess
+from .for_loop import ForEach
+from .for_loop import ForLoop
+from .group_action import GroupAction
+from .include_launch_description import IncludeLaunchDescription
+from .log import Log
+from .log import LogDebug
+from .log import LogError
+from .log import LogInfo
+from .log import LogWarning
+from .opaque_coroutine import OpaqueCoroutine
+from .opaque_function import OpaqueFunction
+from .pop_environment import PopEnvironment
+from .pop_launch_configurations import PopLaunchConfigurations
+from .push_environment import PushEnvironment
+from .push_launch_configurations import PushLaunchConfigurations
+from .register_event_handler import RegisterEventHandler
+from .replace_environment_variables import ReplaceEnvironmentVariables
+from .reset_environment import ResetEnvironment
+from .reset_launch_configurations import ResetLaunchConfigurations
+from .set_environment_variable import SetEnvironmentVariable
+from .set_launch_configuration import SetLaunchConfiguration
+from .shutdown_action import Shutdown
+from .timer_action import TimerAction
+from .unregister_event_handler import UnregisterEventHandler
+from .unset_environment_variable import UnsetEnvironmentVariable
+from .unset_launch_configuration import UnsetLaunchConfiguration
+
+__all__ = [
+    'AppendEnvironmentVariable',
+    'DeclareLaunchArgument',
+    'EmitEvent',
+    'ExecuteLocal',
+    'ExecuteProcess',
+    'ForEach',
+    'ForLoop',
+    'GroupAction',
+    'IncludeLaunchDescription',
+    'Log',
+    'LogDebug',
+    'LogError',
+    'LogInfo',
+    'LogWarning',
+    'OpaqueCoroutine',
+    'OpaqueFunction',
+    'PopEnvironment',
+    'PopLaunchConfigurations',
+    'PushEnvironment',
+    'PushLaunchConfigurations',
+    'ResetEnvironment',
+    'ResetLaunchConfigurations',
+    'RegisterEventHandler',
+    'ReplaceEnvironmentVariables',
+    'SetEnvironmentVariable',
+    'SetLaunchConfiguration',
+    'Shutdown',
+    'TimerAction',
+    'UnregisterEventHandler',
+    'UnsetEnvironmentVariable',
+    'UnsetLaunchConfiguration',
+]
